7ae466e66878c4775c2bf2d5b43514f2a33b9894
Mithlesh Kumar <mithleshji2021@gmail.com>
2025-06-19 00:28:53 +0530

    final commit

--- src/components/TestiMonialSlider.jsx
+++ src/components/TestiMonialSlider.jsx
@@ -1,53 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
+import Frame3 from '../assets/Frame3.svg'
 
-
-const images = [
-  'https://source.unsplash.com/random/800x400?nature',
-  'https://source.unsplash.com/random/800x400?water',
-  'https://source.unsplash.com/random/800x400?sky',
-  'https://source.unsplash.com/random/800x400?forest'
-];
 
 const TestimonialSlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const prevSlide = () => {
-    const isFirst = currentIndex === 0;
-    setCurrentIndex(isFirst ? images.length - 1 : currentIndex - 1);
-  };
-
-  const nextSlide = () => {
-    const isLast = currentIndex === images.length - 1;
-    setCurrentIndex(isLast ? 0 : currentIndex + 1);
-  };
-
   return (
-    <div className="relative w-full max-w-3xl mx-auto overflow-hidden">
-      <div className="flex transition-transform duration-500"
-           style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-        {images.map((src, index) => (
-          <img key={index} src={src} alt={`Slide ${index}`}
-               className="min-w-full h-64 object-cover" />
-        ))}
+    <>
+    <div className="pb-16 pt-16">
+      <div>
+        <img src={Frame3} alt="img" />
       </div>
-
-      {/* Left button */}
-      <button
-        onClick={prevSlide}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white/70 text-black p-2 rounded-full hover:bg-white transition"
-      >
-        &#8592;
-      </button>
-
-      {/* Right button */}
-      <button
-        onClick={nextSlide}
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/70 text-black p-2 rounded-full hover:bg-white transition"
-      >
-        &#8594;
-      </button>
+      <div className="flex justify-center items-center gap-8 pt-8">
+        <div className="h-8 w-8 bg-gray-400 text-black flex justify-center items-center rounded-full hover:bg-white cursor-pointer">
+        <i className=" ri-arrow-left-line"></i>
+        </div>
+        <div className="h-8 w-8 bg-white text-black flex justify-center items-center rounded-full hover:bg-gray-400 cursor-pointer">
+        <i className="ri-arrow-right-line"></i>
+        </div>
+        
+      </div>
     </div>
-  );
+    </>
+  )
+  
 };
 
 export default TestimonialSlider;
